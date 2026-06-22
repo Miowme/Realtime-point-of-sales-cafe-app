9735b9b1896d44c0409d699d0ecbff5c237b5056
William Susanto <williamsusantolukman@gmail.com>
2026-06-22 18:19:30 +0700

    setup project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { DarkModeToggle } from "@/components/common/darkmode-toggle";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>hello</Button>
+      <DarkModeToggle />
+    </div>
+  );
+}
